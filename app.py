5c36459e8791640efb14e45963c53cd6954c3d1b
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-07-30 22:20:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import gdown
 from modules import eda, clustering, visualization
 
 st.set_page_config(page_title="Customer Segmentation App", layout="wide")
@@ -8,7 +9,16 @@ st.title("🛍️ Customer Segmentation for E-Commerce")
 
 @st.cache_data
 def load_data():
-    df = pd.read_csv("data/customer.csv")
+    
+    file_id = '1kin915XqIlc5pSuyDjp6nuc-QXkDx-d6'
+    url = f'https://drive.google.com/uc?id={file_id}'
+    output = 'data.csv'
+    
+    # Download the file if not already present
+    if not os.path.exists(output):
+        gdown.download(url, output, quiet=False)
+    # Load and prepare the data
+    df = pd.read_csv(output)
     df = df[~df['InvoiceNo'].astype(str).str.startswith(('C', 'A'))]
     df = df[df['Quantity'].astype(int) >= 0]
     df.columns = df.columns.str.strip()
