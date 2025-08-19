622a123f9229590b7d3271bf74711262fabedd42
Seriphap Siangnok (Way) <siangnok@gmail.com>
2025-08-19 10:43:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,24 +8,32 @@ st.set_page_config(page_title="Customer Segmentation App", layout="wide")
 
 st.title("🛍️ Customer Segmentation for E-Commerce")
 
+import streamlit as st
+import pandas as pd
+import os
+import gdown
+
 @st.cache_data
-def load_data():
-    
-    file_id = '1kin915XqIlc5pSuyDjp6nuc-QXkDx-d6'
-    url = f'https://drive.google.com/uc?id={file_id}'
-    output = 'data.csv'
-    
-    # Download the file if not already present
-    if not os.path.exists(output):
-        gdown.download(url, output, quiet=False)
-    # Load and prepare the data
-    df = pd.read_csv(output)
+def load_data(uploaded_file=None):
+    if uploaded_file is not None:
+        df = pd.read_csv(uploaded_file)
+    else:
+        file_id = '1kin915XqIlc5pSuyDjp6nuc-QXkDx-d6'
+        url = f'https://drive.google.com/uc?id={file_id}'
+        output = 'data.csv'
+        if not os.path.exists(output):
+            gdown.download(url, output, quiet=False)
+        df = pd.read_csv(output)
+
     df = df[~df['InvoiceNo'].astype(str).str.startswith(('C', 'A'))]
     df = df[df['Quantity'].astype(int) >= 0]
     df.columns = df.columns.str.strip()
     df['InvoiceDate'] = pd.to_datetime(df['InvoiceDate'])
     df['TotalPrice'] = df['Quantity'] * df['UnitPrice']
     return df
+# UI สำหรับอัปโหลดไฟล์
+uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
+df = load_data(uploaded_file)
 
 df = load_data()
 
@@ -37,4 +45,5 @@ elif menu == "Clustering":
     clustering.run(df)
 elif menu == "Visualization":
     visualization.run(df)
+
 
